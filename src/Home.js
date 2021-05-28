cbe969a438d8a7711ff1284fe896f6f72d36db69
ssuryavanshi <ssuryavanshi@atlassian.com>
2021-05-28 14:16:39 +0530

    master commit

--- src/Home.js
+++ src/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
-          <p>Written By : {blog.author}</p>
+          <p>Written : {blog.author}</p>
         </div>
       ))}
     </div>
